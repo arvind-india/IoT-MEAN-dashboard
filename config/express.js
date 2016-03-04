c416531bd06c0a7b2faa6c12b4c7ac9dc61a3ade
Steven Bell <thestevenbell@gmail.com>
2016-03-04 09:55:34 -0500

    added public/components to config/express.js file to try to fix 404 broken links in heroku deploy

--- config/express.js
+++ config/express.js
@@ -25,6 +25,7 @@ module.exports = function(app, config) {
   app.use(cookieParser());
   app.use(compress());
   app.use(express.static(config.root + '/public'));
+  app.use(express.static(config.root + '/public/components'));
   app.use(express.static(config.root + '/client'));
   app.use(methodOverride());
 
